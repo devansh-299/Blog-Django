9139170ebc43352f8e9c24bdc8f31748c8a5bc1e
Devansh-299 <f20180131@goa.bits-pilani.ac.in>
2019-10-15 22:21:51 +0530

    Django practice Blog app

--- blog/models.py
+++ blog/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.conf import settings
 from django.utils import timezone
 
-class Post (models.Model):       # here we defined that our object Post is a django model and has to be saved in database
+class Post (models.Model):       # here we defined that our object Post is a django model and has to be saved in database  , in python bracket has parent class
 
 	# now here we will be specifying the attributes of our object model!
 
@@ -11,7 +11,7 @@ class Post (models.Model):       # here we defined that our object Post is a dja
 	text = models.TextField()						# text without upper limit
 	created_date = models.DateTimeField(default=timezone.now)
 	published_date = models.DateTimeField(blank=True, null=True)
-	def publish(self):
+	def publish(self):                               # we call this method when we want to publish the blog post and hence get the time of that instance
 		self.published_date = timezone.now()
 		self.save()
 	def __str__(self):
--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views      # . for parent directory!
 urlpatterns  = [
 	path('',views.post_list,name='post_list'),
+	 path('post/<int:pk>/', views.post_detail, name='post_detail'),	  # I guess here int: pk is that we receive an integer that is primary key number of the given post
 ]
 
