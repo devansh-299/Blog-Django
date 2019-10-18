677879ba3b8a1bfc58345098f1e0d8e48af55cf9
Devansh-299 <f20180131@goa.bits-pilani.ac.in>
2019-10-18 20:03:29 +0530

    homepage added

--- blog/views.py
+++ blog/views.py
@@ -10,7 +10,7 @@ from django.shortcuts import render,get_object_or_404
 def post_list(request):
 	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
 	return render (request,'blog/post_list.html',{'posts': posts})        # note this posts returns all the filtered posts in List forn!
-	
+	# here we didn't need to specify exactly the template folder bcz django automatically searches for the folder
 	
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)          # i guess this get_object_or_404 is django's built in method!
--- mysite/urls.py
+++ mysite/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('blog.urls')),
+    path('', views.homepage),
+    path('blogs/',include('blog.urls')),  		# registering the blog app urls inside our main projecet
 ]
